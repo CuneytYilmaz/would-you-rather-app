428ba695ed437ec96210974f90b34c2fe772cc4a
CuneytYilmaz <cuneyilmaz@gmail.com>
2019-11-16 22:33:06 +0000

    Action for answer question completed

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,9 @@
-import { saveQuestion } from '../utils/api';
+import { saveQuestion, saveQuestionAnswer } from '../utils/api';
 import { showLoading, hideLoading } from 'react-redux-loading';
 
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS';
 export const ADD_QUESTION = 'ADD_QUESTION';
+export const ANSWER_QUESTION = 'ANSWER_QUESTION';
 
 export function receiveQuestions (questions) {
 	return {
@@ -30,6 +31,30 @@ export function handleAddQuestion (optionOne, optionTwo) {
           	author : authedUser,
         })
       	.then((question) => dispatch(addQuestion(question)))
+      	.then(() => dispatch(hideLoading()))
+    }
+}
+
+export function answerQuestion (authedUser, qid, answer) {
+	return {
+    	type: ANSWER_QUESTION,
+      	authedUser,
+      	qid,
+      	answer
+    }
+}
+
+export function handleAnswerQuestion (qid, answer) {
+	return (dispatch, getState) => {
+    	const { authedUser } = getState();
+      
+      	dispatch(showLoading());
+      	return saveQuestionAnswer({
+        	authedUser,
+          	qid,
+          	answer
+        })
+      	.then(({ authedUser, qid, answer }) => dispatch(answerQuestion(authedUser, qid, answer)))
       	.then(() => dispatch(hideLoading()))
     }
 }
